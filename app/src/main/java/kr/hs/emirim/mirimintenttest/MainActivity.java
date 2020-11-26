e29ffcf98a51cca571954cfb672d30ea4e9a1ac6
dwrdd <s2019w30@e-mirim.hs.kr>
2020-11-26 20:13:05 +0900

    complete

--- app/src/main/java/kr/hs/emirim/mirimintenttest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/mirimintenttest/MainActivity.java
@@ -2,13 +2,61 @@ package kr.hs.emirim.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ed1;
+    EditText ed2;
+    RadioGroup rg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.first);
+        ed2 = findViewById(R.id.se);
+        rg = findViewById(R.id.group);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                switch(rg.getCheckedRadioButtonId()){
+                    case R.id.plus:
+                        intent.putExtra("cal","+");
+                        break;
+                    case R.id.minus:
+                        intent.putExtra("cal","-");
+                        break;
+                    case R.id.mul:
+                        intent.putExtra("cal","*");
+                        break;
+                    case R.id.na:
+                        intent.putExtra("cal","/");
+                        break;
+                }
+                intent.putExtra("num1", Integer.parseInt(ed1.getText().toString()));
+                intent.putExtra("num2", Integer.parseInt(ed2.getText().toString()));
+
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            int hap = data.getIntExtra("Hap", 0);
+            Toast.makeText(getApplicationContext(), "결과" +hap, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/kr/hs/emirim/mirimintenttest/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/mirimintenttest/ResultActivity.java
@@ -2,7 +2,11 @@ package kr.hs.emirim.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        TextView tr=findViewById(R.id.result);
+
+        Intent intent = getIntent();
+        int input1 = Integer.parseInt(intent.getStringExtra("input1"));
+        int input2 = Integer.parseInt(intent.getStringExtra("input2"));
+
+        String result = "";
+        String op = intent.getStringExtra("op");
+        switch (op){
+            case "+":
+                result = input1 + "+"+input2 +"="+ (input1+input2);
+                break;
+            case "-":
+                result = input1 + "-"+input2 +"="+ (input1-input2);
+                break;
+            case "*":
+                result = input1 + "*"+input2 +"="+ (input1*input2);
+                break;
+            case "/":
+                result = input1 + "/"+input2 +"="+ (input1/input2);
+                break;
+        }
+        tr.setText(result);
+
+
+        Button btnback = findViewById(R.id.goback);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
 }
